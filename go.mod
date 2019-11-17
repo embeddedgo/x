279f06544d7effdd631af637fe79dd9e5049bea1
Michał Derkacz <michal@Lnet.pl>
2019-11-17 11:49:17 +0100

    time: add Set function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/embeddedgo/x
+
+go 1.13
---
+++ time/time_noos.go
@@ -0,0 +1,13 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package time
+
+import _ "unsafe"
+
+// Set sets the current time.
+func Set(t *Time) { runtime_setwalltime(t.sec(), t.nsec()) }
+
+//go:linkname runtime_setwalltime runtime.setwalltime
+func runtime_setwalltime(sec int64, nsec int32)
