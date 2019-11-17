46b2b8cb1ca6ac19cd71a3175a29c2e91c62d847
Michał Derkacz <michal@Lnet.pl>
2019-11-17 23:16:42 +0100

    time: fix Set function

--- time/time_noos.go
+++ time/time_noos.go
@@ -7,7 +7,9 @@ package time
 import _ "unsafe"
 
 // Set sets the current time.
-func Set(t Time) { runtime_setwalltime(t.sec(), t.nsec()) }
+func Set(t Time) {
+	runtime_setwalltime(t.sec()+internalToUnix, t.nsec())
+}
 
 //go:linkname runtime_setwalltime runtime.setwalltime
 func runtime_setwalltime(sec int64, nsec int32)
